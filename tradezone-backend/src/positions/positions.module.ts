b1b2c3995cbb8eb86de7d55f738a1fb7ac15433a
shreyesh k <shreyeshk@iconnectsolutions.com>
2025-09-13 15:47:45 +0530

    refactor backend code

--- tradezone-backend/src/positions/positions.module.ts
+++ tradezone-backend/src/positions/positions.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { PositionsService } from './positions.service';
 import { PositionsController } from './positions.controller';
 import { DatabaseModule } from '../database/database.module';
 import { MarketDataService } from './market-data.service';
-import { PositionsCronService } from './positions.cron';
 
 @Module({
-  imports: [DatabaseModule, ScheduleModule.forRoot()],
+  imports: [DatabaseModule],
   controllers: [PositionsController],
-  providers: [PositionsService, MarketDataService, PositionsCronService],
+  providers: [PositionsService, MarketDataService],
   exports: [PositionsService],
 })
 export class PositionsModule {}
